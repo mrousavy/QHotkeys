9523dd044a22ebf09c6c3fbc0e694083d95e4919
mrousavy <mrousavy@donotreply.github.com>
2018-02-11 18:18:29 +0100

    Implement destructor and #error

--- src/qhotkey.cpp
+++ src/qhotkey.cpp
@@ -1,16 +1,23 @@
 #include "qhotkey.h"
+#include <QtCore>
 
 #ifdef Q_OS_WIN
-#include <windows.h>
+#include <Windows.h>
 #elif defined(Q_OS_OSX)
 #include <aqua>
 #elif defined(Q_OS_LINUX)
 #include <X11>
 #endif
 
-size_t QHotkey::_ghkid = 0;
+// TODO: REMOVE
+#include <QDebug>
 
-QHotkey::QHotkey(const Modifier modifiers, const Qt::Key key,
+
+namespace Qt
+{
+int QHotkey::_ghkid = 0;
+
+QHotkey::QHotkey(const ModifierKey modifiers, const Qt::Key key,
                  const callback_t callback)
     : _modifiers(modifiers), _key(key),
       _callback(callback), _hkid(_ghkid++)
@@ -18,47 +25,68 @@ QHotkey::QHotkey(const Modifier modifiers, const Qt::Key key,
     registerHotkey();
 }
 
+QHotkey::~QHotkey()
+{
+#ifdef Q_OS_WIN
+    UnregisterHotKey(NULL, _hkid);
+#elif defined(Q_OS_OSX)
+    // TODO: OSX IMPLEMENTATION
+#elif defined(Q_OS_LINUX)
+    // TODO: LINUX IMPLEMENTATION
+    int XUngrabKey(Display *display, int keycode, unsigned int
+    modifiers, Window grab_window);
+#else
+#error This OS is not supported. You can implement this yourself at https://github.com/mrousavy/QHotkeys
+#endif
+}
+
 void QHotkey::messageLoop() const
 {
 #ifdef Q_OS_WIN
     MSG msg;
-    while (GetMessage(&msg, 0, 0, 0))
-    {
-        switch (msg.message)
-        {
-        case WM_HOTKEY:
-            if (msg.wParam == _hkid)
-            {
+    while (GetMessage(&msg, 0, 0, 0)) {
+        qDebug() << "Message!";
+        if (msg.message == WM_HOTKEY &&
+            msg.wParam == _hkid) {
+            qDebug() << "WM_HOTKEY!";
+            if (msg.wParam == _hkid) {
                 _callback(*this);
             }
         }
     }
 #elif defined(Q_OS_OSX)
-    // TODO:
+    // TODO: OSX IMPLEMENTATION
 #elif defined(Q_OS_LINUX)
-    // TODO:
+    // TODO: LINUX IMPLEMENTATION
+#else
+#error This OS is not supported. You can implement this yourself at https://github.com/mrousavy/QHotkeys
 #endif
 }
 
 void QHotkey::registerHotkey() const
 {
 #ifdef Q_OS_WIN
-    WINBOOL result = RegisterHotKey(0, _hkid, getMod(_modifiers), getKey(_key));
+    auto result = RegisterHotKey(NULL, _hkid, getMod(_modifiers), getKey(_key));
     if (result == FALSE)
         throw std::runtime_error("Could not register hotkey! " + GetLastError());
 #elif defined(Q_OS_OSX)
-    // TODO:
+    // TODO: OSX IMPLEMENTATION
 #elif defined(Q_OS_LINUX)
-    // TODO:
+    // TODO: LINUX IMPLEMENTATION
+    int XGrabKey(Display *display, int keycode, unsigned int
+    modifiers, Window grab_window, Bool owner_events, int pointer_mode, int keyboard_mode);
+#else
+#error This OS is not supported. You can implement this yourself at https://github.com/mrousavy/QHotkeys
 #endif
 }
 
 
-int QHotkey::getMod(const Modifier& modifier) const noexcept
+int QHotkey::getMod(const ModifierKey& modifier) noexcept
 {
     return static_cast<int>(modifier);
 }
-int QHotkey::getKey(const Qt::Key& key) const noexcept
+int QHotkey::getKey(const Qt::Key& key) noexcept
 {
     return static_cast<int>(key);
+}
 }
