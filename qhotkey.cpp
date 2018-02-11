c56ca883b8b0d8a62ecd2ac1ff8aa97387b21c94
mrousavy <mrousavy@donotreply.github.com>
2018-02-11 13:36:48 +0100

    Partially implement functions for Q_OS_WIN

--- qhotkey.cpp
+++ qhotkey.cpp
@@ -1,7 +1,64 @@
 #include "qhotkey.h"
 
+#ifdef Q_OS_WIN
+#include <windows.h>
+#elif defined(Q_OS_OSX)
+#include <aqua>
+#elif defined(Q_OS_LINUX)
+#include <X11>
+#endif
 
-QHotkey::QHotkey(Modifier modifiers, Qt::Key key)
-    : _modifiers(modifiers), _key(key)
+size_t QHotkey::_ghkid = 0;
+
+QHotkey::QHotkey(const Modifier modifiers, const Qt::Key key,
+                 const callbackType callback)
+    : _modifiers(modifiers), _key(key),
+      _callback(callback), _hkid(_ghkid++)
 {
+    registerHotkey();
+}
+
+void QHotkey::messageLoop() const
+{
+#ifdef Q_OS_WIN
+    MSG msg;
+    while (GetMessage(&msg, 0, 0, 0))
+    {
+        switch (msg.message)
+        {
+        case WM_HOTKEY:
+            if (msg.wParam == _hkid)
+            {
+                _callback(*this);
+            }
+        }
+    }
+#elif defined(Q_OS_OSX)
+    // TODO:
+#elif defined(Q_OS_LINUX)
+    // TODO:
+#endif
+}
+
+void QHotkey::registerHotkey() const
+{
+#ifdef Q_OS_WIN
+    WINBOOL result = RegisterHotKey(0, _hkid, getMod(_modifiers), getKey(_key));
+    if (result == FALSE)
+        throw std::runtime_error("Could not register hotkey! " + GetLastError());
+#elif defined(Q_OS_OSX)
+    // TODO:
+#elif defined(Q_OS_LINUX)
+    // TODO:
+#endif
+}
+
+
+int QHotkey::getMod(const Modifier& modifier) const noexcept
+{
+    return modifier;
+}
+int QHotkey::getKey(const Qt::Key& key) const noexcept
+{
+    return key;
 }
